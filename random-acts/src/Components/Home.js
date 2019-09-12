dc2690cc8c29c8aa63e2b2df7c5486d7e09f2413
Claire Sinozich <claire.sinozich@gmail.com>
2019-09-12 17:19:49 -0600

    home component, images

---
+++ random-acts/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <img
+        src="https://live.staticflickr.com/65535/48724482457_7086e67d68.jpg"
+        alt="main"
+      />
+      <h1>Give back with Random Acts of Kindness. Sign Up Today</h1>
+    </div>
+  );
+};
+
+export default Home;
